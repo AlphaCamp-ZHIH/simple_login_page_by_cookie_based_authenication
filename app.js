5a39f6be9fff49e774a7f82103a2b559985e302c
zhih <fufong79570@gmail.com>
2021-04-20 23:50:57 +0800

    Add cookie-based authenication

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 const accountsData = require("./accounts.json");
 
@@ -11,24 +12,32 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
-
+//set session and cookie
+app.use(
+  session({
+    secret: "keyboard cat",
+    cookie: {},
+  })
+);
+//get login page
 app.get("/login", (req, res) => {
   res.render("index", { pageTitle: "login" });
 });
-
+//post email and password
 app.post("/login", (req, res) => {
   const email = req.body.account;
   const password = req.body.password;
   const user = accountsData.find((account) => account.email === email);
-  // console.log(user.password);
   let message = "";
-  // let status = "false"
+
   if (!req.body.account || !req.body.password) {
     message = "Please enter your email and password";
     wrongMessage = "yes";
   }
   if (user) {
     if (user.password === password) {
+      //sign data in session to identify login or not
+      req.session.name = user.firstName;
       return res.redirect(`/${user.firstName}`);
     } else if (!req.body.password) {
       wrongMessage = "yes";
@@ -46,8 +55,13 @@ app.post("/login", (req, res) => {
 
 app.get("/:name", (req, res) => {
   const name = req.params.name;
-  console.log(name)
-  res.render("login", { pageTitle: name, message: name });
+  //if req includes singed data , transfer to /name
+  if (req.session.name) {
+    res.render("login", { pageTitle: name, message: name });
+  } else {
+    //if not , back to login page
+    res.redirect("/login");
+  }
 });
 app.listen(port, () => {
   console.log(`operate http://localhost:${port}`);
